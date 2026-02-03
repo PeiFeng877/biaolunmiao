36e514914731138b24638252ee871ab6de5d2557
peifeng <w>
2026-02-03 19:47:49 +0800

    Initial Commit

---
+++ BianLunMiao/BianLunMiaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BianLunMiaoApp.swift
+//  BianLunMiao
+//
+//  Created by Icarus on 2026/2/3.
+//
+
+import SwiftUI
+
+@main
+struct BianLunMiaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BianLunMiaoTests/BianLunMiaoTests.swift
@@ -0,0 +1,16 @@
+//
+//  BianLunMiaoTests.swift
+//  BianLunMiaoTests
+//
+//  Created by Icarus on 2026/2/3.
+//
+
+import Testing
+
+struct BianLunMiaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
